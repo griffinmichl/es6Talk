209ebe2f477972db3c6e859e94b5a59edf5c7c3b
Zachary Lester <zachary.greg.lester@gmail.com>
2016-04-28 03:23:12 -0700

    Fix spacing in arrow prompt

--- arrow.js
+++ arrow.js
@@ -1,30 +1,30 @@
 // Example:
 {
-	var project = {
-		owner: 'Griffin',
-		todos: ['learn ES6', 'take out the trash', 'fix my remind method'],
-		taskReminder: function() {
-			this.todos.forEach(function(todo) {
-				console.log('Hey, ' + this.owner + '! Be sure to ' + todo + '!');
-			});
-		}
-	};
+var project = {
+  owner: 'Griffin',
+  todos: ['learn ES6', 'take out the trash', 'fix my remind method'],
+  taskReminder: function() {
+    this.todos.forEach(function(todo) {
+      console.log('Hey, ' + this.owner + '! Be sure to ' + todo + '!');
+    });
+  }
+};
 
-	project.taskReminder();
+project.taskReminder();
 }
 
 // Excercise: Fix Dade's procrastinate method so it logs the correct properties.
 {
-	var dade = {
-		name: 'Zero Cool',
-		hobby: 'hack the planet',
-		procrastinate: function() {
-			console.log('Hmm...');
-			setTimeout(function() {
-				console.log('Maybe ' + this.name + ' will '+ this.hobby + ' tomorrow.')
-			}, 1000)
-		}
-	};
+  var dade = {
+    name: 'Zero Cool',
+    hobby: 'hack the planet',
+    procrastinate: function() {
+      console.log('Hmm...');
+      setTimeout(function() {
+        console.log('Maybe ' + this.name + ' will '+ this.hobby + ' tomorrow.')
+      }, 1000)
+    }
+  };
 
-	dade.procrastinate();
+  dade.procrastinate();
 }
