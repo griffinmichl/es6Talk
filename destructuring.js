45922b3e775568d4ecc095a7918c53a8ae4ca3be
Zachary Lester <zachary.greg.lester@gmail.com>
2016-04-28 03:20:13 -0700

    Add arrow and destructuring prompts

---
+++ destructuring.js
@@ -0,0 +1,47 @@
+// Example
+{
+  var module = {
+    baz: function() {
+      console.log('baz');
+    },
+    qux: function() {
+      console.log('qux');
+    }
+  };
+
+  // Instead of...
+  var baz = module.baz;
+  var qux = module.qux;
+
+  // We can now do...
+  var { baz, qux } = module;
+  baz();
+  qux();
+
+  // This works for arrays, too!
+  var arr = ['foo', 'bar'];
+  var foo = arr[0];
+  var bar = arr[1];
+
+  var [foo, bar] = arr;
+
+  // You'll commonly see this pattern in import/require statements:
+  import { render } from 'react-dom';
+  const { static, Router } = require('express');
+}
+
+// Excercise: Convert the following into ES6 destructuring assignments!
+{
+  var mentor = {
+    name: 'Sunny',
+    likes: ['Meteor', 'Bitcoin']
+  };
+
+  var name = mentor.name;
+  var likes = mentor.likes;
+
+  var hirs = ['Alexxis', 'Jake (good)', 'Jake (evil)', 'Griffin', 'Ian'];
+  var alexxis = hirs[0];
+  var goodJake = hirs[1];
+  var evilJake = hirs[2];
+}
